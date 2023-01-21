3c87c900f782000d7e11cd081abedf819a641216
ngohongthong <baphongpine@gmail.com>
2023-01-21 22:18:53 +0700

    completed UI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 function App() {
   
   const [api,setApi] = useState([])
 
   const apiHTTP = "http://localhost:3333/"
 
-  useEffect(()=>{
-    fetch(`${apiHTTP}posts`)
-    .then((response) => response.json())
-    .then((data) => setApi(data));
-  },[])
+
 
 
   console.log(api)
 
+  const handleFetch = () =>{
+      fetch(`${apiHTTP}posts`)
+      .then((response) => response.json())
+      .then((data) => setApi(data));
+  }
+
+  const  handleADD = ()=>{
+    console.log("handleADD")
+  }
+  const    handleUPGRADE = ()=>{
+    console.log("handleUPGRADE")
+  }
+  const    handleDELETE = ()=>{
+    console.log("handleDELETE")
+  }
 
 
   return (
     <div className="App">
       <h1>LEARN JSONsever</h1>
       <ul>
-        {api.map( item => <h4 key={item.id}> {item.title}</h4>)}
+        <button style={{margin : '5px'}}  onClick = {handleFetch}>Fetch Data</button>
+        <button style={{margin : '5px'}}  onClick = {handleADD}>ADD Data</button>
+  
+        {api.map( item => <div key={item.id}>
+          <h4> {item.title}</h4>
+          <button style={{margin : '5px'}}  onClick = {handleUPGRADE}>UPGRADE Data</button>
+          <button style={{margin : '5px'}}  onClick = {handleDELETE}>DELETE Data</button>
+        </div>)}
       </ul>
-      <button style={{margin : '5px'}}>ADD Data</button>
-      <button style={{margin : '5px'}}>UPGRADE Data</button>
-      <button style={{margin : '5px'}}>DELETE Data</button>
     </div>
   );
 }
